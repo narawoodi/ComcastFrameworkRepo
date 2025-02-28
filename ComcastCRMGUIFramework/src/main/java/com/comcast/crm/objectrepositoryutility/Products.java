7d56f2c501a52864c8f54f25370ac59f38557f43
DELL <DELL@NARAHARI-WOODI-DELL>
2025-02-28 10:58:25 +0530

    Added Product Link in the Homepage

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
@@ -4,11 +4,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class Products {
-	@FindBy(xpath = "//img[img[@alt='Create Product...']")
+	@FindBy(xpath = "//img[@alt='Create Product...']")
 	private WebElement createProductImgBtn;
-	
-	@FindBy(name = "search")
-	private WebElement ele2;
 }
 
 
