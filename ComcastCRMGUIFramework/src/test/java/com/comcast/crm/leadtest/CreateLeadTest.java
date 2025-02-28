f564c09bd3403d27b9d6497f94eba53c3e971ad0
DELL <DELL@NARAHARI-WOODI-DELL>
2025-02-28 16:19:15 +0530

    Updated some lines of codes

--- ComcastCRMGUIFramework/src/test/java/com/comcast/crm/leadtest/CreateLeadTest.java
+++ ComcastCRMGUIFramework/src/test/java/com/comcast/crm/leadtest/CreateLeadTest.java
@@ -57,4 +57,45 @@ public class CreateLeadTest extends BaseClass{
 			System.out.println(companyName+" entered is invalid--FAIL");
 		}		
 	}
+	
+	@Test
+	public void createLeadWithPhoneNumberTest() throws Throwable {
+		String leadName = eLib.getDataFromExcel("lead", 4, 2)+jLib.getRandomNumber();
+		String companyName = eLib.getDataFromExcel("lead", 4, 3);	
+		String phoneNumber = eLib.getDataFromExcel("lead", 4, 4);
+		/*Step 2: Navigate to Leads Module*/
+		driver.findElement(By.linkText("Leads")).click();
+		 
+		/*Step 3: Click on "Create Lead" Button*/
+		driver.findElement(By.xpath("//img[@alt='Create Lead...']")).click();
+		
+		/*Step 4: Enter all the details and create new Lead*/
+		driver.findElement(By.name("lastname")).sendKeys(leadName);
+		driver.findElement(By.name("company")).sendKeys(companyName);
+		driver.findElement(By.id("phone")).sendKeys(phoneNumber);
+		driver.findElement(By.xpath("//input[@title='Save [Alt+S]']")).click();
+		
+		/*Step 5: Verify whether the entered details is correct or not*/
+		String actLeadName = driver.findElement(By.id("dtlview_Last Name")).getText();
+		if (actLeadName.equals(leadName)) {
+			System.out.println(leadName+" entered is valid--PASS");
+		} else {
+			System.out.println(leadName+" entered is invalid--FAIL");
+		}
+		
+		String actCompanyName = driver.findElement(By.id("dtlview_Company")).getText();
+		if (actCompanyName.equals(companyName)) {
+			System.out.println(companyName+" entered is valid--PASS");
+		} else {
+			System.out.println(companyName+" entered is invalid--FAIL");
+		}
+		
+		String actPhoneNumber = driver.findElement(By.id("dtlview_Phone")).getText();
+		if (actPhoneNumber.equals(phoneNumber)) {
+			System.out.println(phoneNumber+" entered is valid--PASS");
+		} else {
+			System.out.println(phoneNumber+" entered is invalid-FAIL");
+		}
+
+	}
 }
